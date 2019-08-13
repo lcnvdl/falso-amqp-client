5696eaf27e038ba6f06441512c2c85a97d5808dd
Lcnvdl <programando.soft@gmail.com>
2019-08-12 23:33:41 -0300

    Update protocol-v1.js

--- src/protocols/protocol-v1.js
+++ src/protocols/protocol-v1.js
@@ -1,15 +1,19 @@
 const JsonHelper = require("../helpers/json.helper");
 
 class ProtocolV1 {
+    /**
+     * Parses a message.
+     * @param {string} msg Message
+     */
     static parse(msg) {
         if (msg[0] === "b" && msg[1] === "|") {
             msg = JsonHelper.stringFromB64(msg.substr(2)).trim();
         }
 
-        cmd = msg.substr(0, msg.indexOf("|"));
+        let cmd = msg.substr(0, msg.indexOf("|"));
         msg = msg.substr(msg.indexOf("|") + 1);
 
-        msgID = cmd.split(",")[1];
+        let msgID = cmd.split(",")[1];
         cmd = cmd.split(",")[0];
 
         if (msg[0] === "{") {
@@ -23,11 +27,18 @@ class ProtocolV1 {
         throw new Error("Invalid package");
     }
 
+    /**
+     * Serializes a message.
+     * @param {string} cmd Command
+     * @param {*} obj Object to serialize
+     * @param {string|number} [replyTo] Message ID (for answering)
+     */
     static prepare(cmd, obj, replyTo) {
         if (replyTo) {
             cmd = cmd + "," + replyTo;
         }
-        msg = cmd + "|" + JSON.stringify(obj);
+
+        let msg = cmd + "|" + JSON.stringify(obj);
         msg = "b|" + JsonHelper.toB64(msg);
         return msg;
     }
